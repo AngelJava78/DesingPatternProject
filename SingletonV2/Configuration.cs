56a9da4a5d7a7f96a04f78c0debd07a1356c0ba0
Angel Javier <angel_java@hotmail.com>
2021-10-09 02:09:18 -0500

    Singleton Design Pattern was added to project

---
+++ SingletonV2/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonV2
+{
+    public class Configuration
+    {
+        private static Configuration instance;
+        public string AppName { get; set; }
+        public  string AppVersion { get; set; }
+        public  string DateFormat { get; set; }
+        public  string CurrencyFormat { get; set; }
+        private Configuration()
+        {
+            AppName = ConfigurationManager.AppSettings["appName"];
+            AppVersion = ConfigurationManager.AppSettings["appVersion"];
+            DateFormat = ConfigurationManager.AppSettings["dateFormat"];
+            CurrencyFormat = ConfigurationManager.AppSettings["currencyFormat"];
+        }
+        public static Configuration GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Configuration();
+            }
+            return instance;
+        }
+
+        
+    }
+}
---
+++ SingletonV2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var config1 = Configuration.GetInstance();
+            var config2 = Configuration.GetInstance();
+            Console.WriteLine($"Are config1 and config2 equals? {config1.Equals(config2)}");
+            Console.WriteLine($"Config1.AppName: {config1.AppName}");
+            Console.WriteLine($"Config1.AppVersion: {config1.AppVersion}");
+            Console.WriteLine($"Config1.DateFormat: {config1.DateFormat}");
+            Console.WriteLine($"Config1.CurrencyFormat: {config1.CurrencyFormat}");
+            config1.AppName = "Singleton app v1.0.1";
+            config1.AppVersion = "v1.0.1";
+            config1.DateFormat = "yyyy-MM-dd";
+            config1.CurrencyFormat = "#0.00";
+            Console.WriteLine($"Config2.AppName: {config2.AppName}");
+            Console.WriteLine($"Config2.AppVersion: {config2.AppVersion}");
+            Console.WriteLine($"Config2.DateFormat: {config2.DateFormat}");
+            Console.WriteLine($"Config2.CurrencyFormat: {config2.CurrencyFormat}");
+
+        }
+    }
+}
