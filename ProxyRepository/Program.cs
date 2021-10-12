c31a00459b87d736f816b3c370925d98c1e8b77a
Angel Javier <angel_java@hotmail.com>
2021-10-12 14:08:03 -0500

    Second example of Proxy design pattern

---
+++ ProxyRepository/Program.cs
@@ -0,0 +1,36 @@
+﻿using ProxyRepository.Data;
+using ProxyRepository.Entities;
+using ProxyRepository.Proxy;
+using System;
+
+namespace ProxyRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var repository = new CustomerRepositoryProxy();
+            Session.CanSave = true;
+            Session.CanGetAll = true;
+            var customer1 = new Customer { Name = "Customer 1" };
+            var customer2 = new Customer { Name = "Customer 2" };
+            try
+            {
+                repository.Save(customer1);
+                repository.Save(customer2);
+                foreach (var c in repository.GetAll())
+                {
+                    Console.WriteLine($"Customer name: {c.Name}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.GetType()}");
+                Console.WriteLine("Message:");
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
---
+++ ProxyRepository/Proxy/CustomerRepositoryProxy.cs
@@ -0,0 +1,49 @@
+﻿using ProxyRepository.Data;
+using ProxyRepository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyRepository.Proxy
+{
+    public class CustomerRepositoryProxy : IRepository
+    {
+        IRepository realRepository;
+
+        public IList<Customer> GetAll()
+        {
+            var result = new List<Customer>();
+            if(realRepository == null)
+            {
+                realRepository = new CustomerRepository();
+            }
+            if(Session.CanGetAll)
+            {
+                result=  realRepository.GetAll().ToList();
+            }
+            else
+            {
+                throw new UnauthorizedAccessException();
+            }
+            return result;
+        }
+
+        public void Save(Customer customer)
+        {
+            if(realRepository == null)
+            {
+                realRepository = new CustomerRepository();
+            }
+            if(Session.CanSave)
+            {
+                realRepository.Save(customer);
+            }
+            else
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
